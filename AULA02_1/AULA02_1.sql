c8b0daed515970b3f7d8ea93c930ae9ce18150fa
VINICIUS PEREIRA SEVERO <RM76743@fiap.com.br>
2017-02-15 11:37:34 -0200

    adicionado script 2

--- AULA02_1/AULA02_1.sql
+++ AULA02_1/AULA02_1.sql
@@ -1,8 +1,10 @@
 cl scr
 SET SERVEROUTPUT ON
 set feed off
+declare 
+  dia int := 15;
 begin
-  DBMS_OUTPUT.PUT_LINE('hello');
+  DBMS_OUTPUT.PUT_LINE('hello world'||' dia '||dia);
 end;
 /
 SET SERVEROUTPUT OFF
